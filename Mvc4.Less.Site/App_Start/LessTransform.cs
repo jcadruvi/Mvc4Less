2ff47010fa0f7f13364b42f212dc27b0378d78c0
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-21 15:01:43 -0700

    Adding less files.

---
+++ Mvc4.Less.Site/App_Start/LessTransform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Mvc4.Less.App_Start
+{
+    public class LessTransform : IBundleTransform
+    {
+        void IBundleTransform.Process(BundleContext context, BundleResponse response)
+        {
+            response.Content = dotless.Core.Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
